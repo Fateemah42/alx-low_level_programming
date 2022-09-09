ade9bdf7d6d67f2bd9bace1b9f08ad4898061c31
Fatima <fateemaaleeyu42@gmail.com>
2022-09-08 17:33:05 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.n>
+#include <stdio.h>
+
 /**
- * main-entry point for the program
+ * main = Entry point
  *
- * Return:0 if no errors, return non zero if errors
+ * Return: Always 0 (Success)
  */
-int main(viod)
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
+}
