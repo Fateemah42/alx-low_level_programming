85dbd270ce175c1f3c9da0641de0e132b0ada899
Fatima <fateemaaleeyu42@gmail.com>
2022-09-10 13:07:57 -0700

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of a pair of
- * two digit numbers, without any repetition.
+ * main - Prints all possible combinations of
+ * two digiit numbers, without any repetition.
  *
- * Return: Always 0.
+ * Return: 0 on success
  */
 int main(void)
 {
-	int c, i, k, j;
+	int i, j;
 
-	for (c = 48; c <= 57; c++)
+	for (i = 0; 1 <= 99; i++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (j = 0; j <= 99; j++)
 		{
-			for (k = 48; k <= 57; k++)
+			if (i < j && i != j)
 			{
-				for (j = 48; j <= 57; j++)
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
-					if (((k + j) > (c + i) && k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
