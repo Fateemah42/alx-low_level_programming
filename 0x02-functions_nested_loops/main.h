5c90346e0141453241a200e04d46a3ae6d8400e1
Fatima <fateemaaleeyu42@gmail.com>
2022-09-13 14:20:11 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef "main.h"
 #define "main.h"
 
-#inlude<stdio.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c);
+int _isalpha(int.c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -16,4 +16,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif         
+#endif
