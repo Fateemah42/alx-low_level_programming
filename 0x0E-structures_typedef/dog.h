0c3dc19b86c9c728b38a16e7d608748ae0c3f94e
Fateemah42 <fateemaaleeyu42@gmail.com>
2022-10-10 12:43:41 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,18 +5,11 @@
 
 
 /**
-
  * struct dog - structure dog
-
  * @name: atribute pointer to char name's dog
-
  * @age: atribute float age's dog
-
  * @owner: atribute pointer to char owner's dog
-
  */
-
-
 
 struct dog
 
